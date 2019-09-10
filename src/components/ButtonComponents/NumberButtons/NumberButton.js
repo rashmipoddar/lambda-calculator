d961c2e8d54011dc84cda29fc3d9d7c578a36186
Rashmi <poddar.rashmi@gmail.com>
2019-09-10 19:06:56 -0400

    Working on styling the calculator

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -4,7 +4,7 @@ const NumberButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button>{props.calculatorNumber}</button>
+      <button className="button numberButtons">{props.calculatorNumber}</button>
     </>
   );
 };
